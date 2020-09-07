33552f8bdba74d0fcad5de6d98b9fe5f81648d1e
Dean J. Bassett Jr <deano92964@gmail.com>
2020-09-07 18:09:56 -0400

    Update upgrade files.

--- upgrade/files/1.0.0-1.1.0/lang_en.php
+++ upgrade/files/1.0.0-1.1.0/lang_en.php
@@ -4,6 +4,9 @@ $aLangContent = array(
 
   // ================ ADDED KEYS ==================
   '_sys_txt_error_you_are_blocked' => 'You cannot view the page because you are blocked by this member.',
+  '_adm_license_note_free' => 'Free License. (Automatically generated)<br />To get rid of the admin area unregistered prompts and the front end powered by links, you will need a Monthly or Yearly license. <a href="https://www.cheetahwsb.com/m/license_server/buy_license/">Click here to purchase a license.</a>',
+  '_adm_license_reset' => 'Reset',
+  '_adm_license_reset_warning' => 'Are you sure you want to reset the license? The current license will be deleted!',  
 
 
   // ================ CHANGED KEYS ==================
--- upgrade/files/1.0.0-1.1.0/sql.sql
+++ upgrade/files/1.0.0-1.1.0/sql.sql
@@ -1,28 +1,5 @@
-UPDATE `sys_menu_top` SET `Caption` = '_ch_polls_profile' WHERE `Parent` = 4 AND `Name` = 'My Polls';
-UPDATE `sys_menu_top` SET `Caption` = '_ch_ads_Ads_profile' WHERE `Parent` = 4 AND `Name` = 'Profile Ads';
-UPDATE `sys_menu_top` SET `Caption` = '_ch_ads_Ads_profile' WHERE `Parent` = 9 AND `Name` = 'Profile Ads';
+DELETE FROM `sys_menu_admin` WHERE `name` = 'mobile_pages';
 
-UPDATE `sys_options` SET `AvailableValues` = 'File,Memcache' WHERE `Name` = 'sys_db_cache_engine';
-UPDATE `sys_options` SET `AvailableValues` = 'File,Memcache' WHERE `Name` = 'sys_pb_cache_engine';
-UPDATE `sys_options` SET `AvailableValues` = 'File,Memcache' WHERE `Name` = 'sys_mm_cache_engine';
-UPDATE `sys_options` SET `AvailableValues` = 'FileHtml,Memcache' WHERE `Name` = 'sys_template_cache_engine';
-
-DELETE FROM `sys_options` WHERE `Name` = 'sys_antispam_smart_check';
-
-INSERT IGNORE INTO `sys_options` VALUES ('sys_antispam_bot_check', '', 23, 'Block Detected Spam Bots', 'checkbox', '', '', 10, '');
-
-UPDATE `sys_options` SET `order_in_kateg` = '10' WHERE `Name` = 'sys_antispam_bot_check';
-UPDATE `sys_options` SET `order_in_kateg` = '20' WHERE `Name` = 'sys_dnsbl_enable';
-UPDATE `sys_options` SET `order_in_kateg` = '30' WHERE `Name` = 'sys_dnsbl_behaviour';
-UPDATE `sys_options` SET `order_in_kateg` = '40' WHERE `Name` = 'sys_uridnsbl_enable';
-UPDATE `sys_options` SET `order_in_kateg` = '50' WHERE `Name` = 'sys_akismet_enable';
-UPDATE `sys_options` SET `order_in_kateg` = '60' WHERE `Name` = 'sys_akismet_api_key';
-UPDATE `sys_options` SET `order_in_kateg` = '70' WHERE `Name` = 'sys_stopforumspam_enable';
-UPDATE `sys_options` SET `order_in_kateg` = '80' WHERE `Name` = 'sys_stopforumspam_api_key';
-UPDATE `sys_options` SET `order_in_kateg` = '90' WHERE `Name` = 'sys_antispam_block';
-UPDATE `sys_options` SET `order_in_kateg` = '100' WHERE `Name` = 'sys_antispam_report';
-UPDATE `sys_options` SET `order_in_kateg` = '110' WHERE `Name` = 'sys_antispam_add_nofollow';
 
 -- last step is to update current version
-
-UPDATE `sys_options` SET `VALUE` = '1.0.0.B3' WHERE `Name` = 'sys_tmp_version';
+UPDATE `sys_options` SET `VALUE` = '1.1.0' WHERE `Name` = 'sys_tmp_version';
