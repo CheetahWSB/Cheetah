9f6ff274948a30a162fd2902b4d3fcb6ab8c3e7b
Dean J. Bassett Jr <deano92964@gmail.com>
2020-09-08 13:18:36 -0400

    Update upgrade files

--- upgrade/files/1.0.0-1.1.0/sql.sql
+++ upgrade/files/1.0.0-1.1.0/sql.sql
@@ -1,5 +1,4 @@
 DELETE FROM `sys_menu_admin` WHERE `name` = 'mobile_pages';
 
-
 -- last step is to update current version
 UPDATE `sys_options` SET `VALUE` = '1.1.0' WHERE `Name` = 'sys_tmp_version';
