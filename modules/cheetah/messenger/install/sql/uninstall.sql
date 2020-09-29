0cc7eeab33179ec9b70509f9d1f40f3ebe12ca9f
Dean J. Bassett Jr <deano@cheetahwsb.com>
2020-09-29 01:41:05 -0400

    Remove messenger. Issue #96

--- modules/cheetah/messenger/install/sql/uninstall.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM `sys_objects_actions` WHERE `Type` = 'Profile' AND `Eval` = 'return ChWsbService::call(''messenger'', ''get_action_link'', array({member_id}, {ID}));';
-
-DELETE FROM `sys_injections` WHERE `name`='messenger_invitation';
-
-DELETE FROM `sys_acl_actions` WHERE `Name`='use messenger';
