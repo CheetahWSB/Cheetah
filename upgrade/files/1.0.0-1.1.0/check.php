5ffd7e152e328e0146b5b63fcf086a893d45d70a
Dean J. Bassett Jr <deano@cheetahwsb.com>
2020-11-17 20:06:38 -0500

    Preparations to upgrade folder contents

--- upgrade/files/1.0.0-1.1.0/check.php
+++ upgrade/files/1.0.0-1.1.0/check.php
@@ -1,12 +1,12 @@
 <?php
 
-if (version_compare(PHP_VERSION, '5.4.0', '<')) 
+if (version_compare(PHP_VERSION, '5.4.0', '<'))
     return "This version requires PHP 5.4.0 or newer";
 
 $mixCheckResult = 'Update can not be applied';
 
 $sCurVer = $this->oDb->getOne("SELECT `VALUE` FROM `sys_options` WHERE `Name` = 'sys_tmp_version'");
 
-if ('1.0.0.B2' == $sCurVer) $mixCheckResult = true;
+if ('1.0.0' == $sCurVer) $mixCheckResult = true;
 
 return $mixCheckResult;
