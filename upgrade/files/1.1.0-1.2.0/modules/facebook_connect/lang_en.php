3c4e4320598a678786188a6b5af9b6666a1fef9c
Dean J. Bassett Jr <deano@cheetahwsb.com>
2021-04-28 13:45:30 -0400

    Fix previous commits error for issue #130

--- upgrade/files/1.1.0-1.2.0/modules/facebook_connect/lang_en.php
+++ upgrade/files/1.1.0-1.2.0/modules/facebook_connect/lang_en.php
@@ -6,6 +6,6 @@ $aLangContent = array(
 
 
   // ================ UPDATED KEYS ==================
-  '_ch_facebook_information_block'       => 'To get your "App ID" and "App Secret", you need to <a href="https://developers.facebook.com/apps/" target="_blank">make Facebook app</a>. <br /><br />Please follow <a href="https://wiki.cheetahwsb.com/index.php?title=Creating_app_on_Facebook_for_Facebook_Connect_Module&action=edit&redlink=1" target="_blank">these instructions</a> on how to create and setup the app. <br /><br />Your "Site URL" for the app: <b>{0}</b> <br />Your "OAuth Redirect URI" for the app: <b>{1}</b> <br /><br />After completing these steps, insert your "App ID" and "App Secret" below:',
+  '_ch_facebook_information_block'       => 'To get your "App ID" and "App Secret", you need to <a href="https://developers.facebook.com/apps/" target="_blank">make Facebook app</a>. <br /><br />Please follow <a href="https://wiki.cheetahwsb.com/index.php?title=Creating_app_on_Facebook_for_Facebook_Connect_Module" target="_blank">these instructions</a> on how to create and setup the app. <br /><br />Your "Site URL" for the app: <b>{0}</b> <br />Your "OAuth Redirect URI" for the app: <b>{1}</b> <br /><br />After completing these steps, insert your "App ID" and "App Secret" below:',
 
 );
