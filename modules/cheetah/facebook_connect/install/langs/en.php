8bdb163f64ae6fce1477fc3afcc11707791fa535
Dean J. Bassett Jr <deano@cheetahwsb.com>
2021-04-28 13:41:57 -0400

    Facebook connect module link to instructions is invalid. #130

--- modules/cheetah/facebook_connect/install/langs/en.php
+++ modules/cheetah/facebook_connect/install/langs/en.php
@@ -10,7 +10,7 @@
         '_ch_facebook'                         => 'Facebook Connect',
         '_ch_facebook_settings'                => 'Facebook Connect',
         '_ch_facebook_information'             => 'Facebook connect information',
-        '_ch_facebook_information_block'       => 'To get your "App ID" and "App Secret", you need to <a href="https://developers.facebook.com/apps/" target="_blank">make Facebook app</a>. <br /><br />Please follow <a href="https://www.cheetahwsb.com/forums/topic/How-to-setup-Facebook-Connect-module.htm" target="_blank">these instructions</a> on how to create and setup the app. <br /><br />Your "Site URL" for the app: <b>{0}</b> <br />Your "OAuth Redirect URI" for the app: <b>{1}</b> <br /><br />After completing these steps, insert your "App ID" and "App Secret" below:',
+        '_ch_facebook_information_block'       => 'To get your "App ID" and "App Secret", you need to <a href="https://developers.facebook.com/apps/" target="_blank">make Facebook app</a>. <br /><br />Please follow <a href="https://wiki.cheetahwsb.com/index.php?title=Creating_app_on_Facebook_for_Facebook_Connect_Module&action=edit&redlink=1" target="_blank">these instructions</a> on how to create and setup the app. <br /><br />Your "Site URL" for the app: <b>{0}</b> <br />Your "OAuth Redirect URI" for the app: <b>{1}</b> <br /><br />After completing these steps, insert your "App ID" and "App Secret" below:',
         '_ch_facebook_profile_error_info'      => 'Can\'t define your profile information',
         '_ch_facebook_profile_error_api_keys'  => 'Please configure the module in admin panel (can\'t define facebook api keys)',
         '_ch_facebook_chart'                   => 'Facebook Profiles',
