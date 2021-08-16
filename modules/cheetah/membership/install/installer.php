91e5b0ecc4fbadb71d89ec10557d873f6a81694d
Dean Bassett <deano92964@gmail.com>
2021-08-16 11:04:06 -0400

    Fixes #173

--- modules/cheetah/membership/install/installer.php
+++ modules/cheetah/membership/install/installer.php
@@ -9,58 +9,57 @@ ch_import("ChWsbInstaller");
 
 class ChMbpInstaller extends ChWsbInstaller
 {
-    function __construct($aConfig)
+    public function __construct($aConfig)
     {
         parent::__construct($aConfig);
         $this->_aActions['check_payment'] = array(
-			'title' => _t('_adm_txt_modules_check_dependencies'),
-		);
+            'title' => _t('_adm_txt_modules_check_dependencies'),
+        );
     }
 
-	function actionCheckPayment($bInstall = true)
-	{
-		if(!$bInstall)
-			return CH_WSB_INSTALLER_SUCCESS;
+    public function actionCheckPayment($bInstall = true)
+    {
+        if (!$bInstall) {
+            return CH_WSB_INSTALLER_SUCCESS;
+        }
 
-		$aError = array('code' => CH_WSB_INSTALLER_FAILED, 'content' => _t('_adm_txt_modules_wrong_dependency_install_payment'));
+        $aError = array('code' => CH_WSB_INSTALLER_FAILED, 'content' => _t('_adm_txt_modules_wrong_dependency_install_payment'));
 
-		$sPayment = getParam('sys_default_payment');
-		if(empty($sPayment))
-			return $aError;
+        $sPayment = getParam('sys_default_payment');
+        if (empty($sPayment)) {
+            return $aError;
+        }
 
-		$oModuleDb = new ChWsbModuleDb();
-		$aPayment = $oModuleDb->getModuleByUri($sPayment);
-		if(empty($aPayment) || !is_array($aPayment))
-			return $aError;
+        $oModuleDb = new ChWsbModuleDb();
+        $aPayment = $oModuleDb->getModuleByUri($sPayment);
+        if (empty($aPayment) || !is_array($aPayment)) {
+            return $aError;
+        }
 
-    	return CH_WSB_INSTALLER_SUCCESS;
+        return CH_WSB_INSTALLER_SUCCESS;
     }
 
-	function actionCheckPaymentFailed($mixedResult)
+    public function actionCheckPaymentFailed($mixedResult)
     {
         return $mixedResult['content'];
     }
 
-	function install($aParams)
+    public function install($aParams)
     {
         $aResult = parent::install($aParams);
 
-		if($aResult['result'] && ChWsbRequest::serviceExists('payment', 'update_dependent_modules'))
+        if ($aResult['result'] && ChWsbRequest::serviceExists('payment', 'update_dependent_modules')) {
             ChWsbService::call('payment', 'update_dependent_modules', array($this->_aConfig['home_uri'], true));
-
-		if($aResult['result'] && ChWsbRequest::serviceExists('payflow', 'update_dependent_modules'))
-            ChWsbService::call('payflow', 'update_dependent_modules', array($this->_aConfig['home_uri'], true));
+        }
 
         return $aResult;
     }
 
-    function uninstall($aParams)
+    public function uninstall($aParams)
     {
-		if(ChWsbRequest::serviceExists('payment', 'update_dependent_modules'))
+        if (ChWsbRequest::serviceExists('payment', 'update_dependent_modules')) {
             ChWsbService::call('payment', 'update_dependent_modules', array($this->_aConfig['home_uri'], false));
-
-		if(ChWsbRequest::serviceExists('payflow', 'update_dependent_modules'))
-            ChWsbService::call('payflow', 'update_dependent_modules', array($this->_aConfig['home_uri'], false));
+        }
 
         return parent::uninstall($aParams);
     }
