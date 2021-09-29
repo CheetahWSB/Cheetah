2fb92c450da4833e5e74eed81bba9dc675668f89
Dean Bassett <deano92964@gmail.com>
2021-09-29 09:27:38 -0400

    Upgrade script changes

--- upgrade/files/1.1.0-1.2.0/modules/photos/sql.sql
+++ upgrade/files/1.1.0-1.2.0/modules/photos/sql.sql
@@ -1,12 +1,3 @@
-SET @iCategId = (SELECT `ID` FROM `sys_options_cats` WHERE `name` = 'Photos' LIMIT 1);
-SET @pos := 0;
-UPDATE `sys_options` SET `order_in_kateg` = ( SELECT @pos := @pos + 10 ) WHERE `kateg` = @iCategId ORDER BY `order_in_kateg`;
-INSERT INTO `sys_options` (`Name`, `VALUE`, `kateg`, `desc`, `Type`, `check`, `err_text`, `order_in_kateg`, `AvailableValues`) VALUES
-('ch_photos_cat_required', 'on', @iCategId, 'Require category when editing', 'checkbox', '', '', '15', '');
-SET @pos := 0;
-UPDATE `sys_options` SET `order_in_kateg` = ( SELECT @pos := @pos + 10 ) WHERE `kateg` = @iCategId ORDER BY `order_in_kateg`;
-UPDATE `sys_options` SET `VALUE` = 'html5', `AvailableValues` = 'html5,embed' WHERE `Name` = 'ch_photos_uploader_switcher';
-
 INSERT INTO `sys_options` (`Name`, `VALUE`, `kateg`, `desc`, `Type`, `check`, `err_text`, `order_in_kateg`, `AvailableValues`) VALUES
 ('ch_photos_delete_orig', '', @iCategId, 'Delete origional photo after upload', 'checkbox', '', '', 32, ''),
 
