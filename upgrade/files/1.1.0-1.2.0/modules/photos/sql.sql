c98625a920f4ff41447b077363d185c9f9155147
Dean Bassett <deano92964@gmail.com>
2021-10-04 23:42:50 -0400

    Upgrade Script Changes

--- upgrade/files/1.1.0-1.2.0/modules/photos/sql.sql
+++ upgrade/files/1.1.0-1.2.0/modules/photos/sql.sql
@@ -1,5 +1,6 @@
+SET @iCategId = (SELECT `ID` FROM `sys_options_cats` WHERE `name` = 'Photos' LIMIT 1);
 INSERT INTO `sys_options` (`Name`, `VALUE`, `kateg`, `desc`, `Type`, `check`, `err_text`, `order_in_kateg`, `AvailableValues`) VALUES
-('ch_photos_delete_orig', '', @iCategId, 'Delete origional photo after upload', 'checkbox', '', '', 32, ''),
+('ch_photos_delete_orig', '', @iCategId, 'Delete origional photo after upload', 'checkbox', '', '', 32, '');
 
 UPDATE `sys_objects_actions` SET `Caption` = '{evalResult}', `Eval` = 'if(getParam(\'ch_photos_delete_orig\') != \'on\') return _t(\'_ch_photos_action_view_original\');' WHERE `Caption` = '_ch_photos_action_view_original';
 
