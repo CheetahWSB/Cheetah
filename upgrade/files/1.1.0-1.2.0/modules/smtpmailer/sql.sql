f646b5fb6541b824e6d54d8285ab85fad8e1e346
Dean Bassett <deano92964@gmail.com>
2021-10-04 23:58:31 -0400

    Upgrade Script Changes. Fixing problems.

--- upgrade/files/1.1.0-1.2.0/modules/smtpmailer/sql.sql
+++ upgrade/files/1.1.0-1.2.0/modules/smtpmailer/sql.sql
@@ -1,4 +1,8 @@
 
 -- update module version
 
+-- Bug in previous version where the version info is not properly set. Need to fix it if it was not manually fixed.
+UPDATE `sys_modules` SET `version` = '1.1.0' WHERE `uri` = 'smtpmailer' AND `version` = '1.010';
+
+-- Now update it to the new version.
 UPDATE `sys_modules` SET `version` = '1.2.0' WHERE `uri` = 'smtpmailer' AND `version` = '1.1.0';
